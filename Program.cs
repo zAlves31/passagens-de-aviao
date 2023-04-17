4f1ebe23edecc7a88d3c73db51a4bce9934483b6
João Victor Alves Bautista <52546124870@info.local>
2023-04-17 10:55:45 -0300

    passagens-de-aviao

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿string senha;
+bool autenticado = false;
+            
+do
+{
+Console.WriteLine("Por favor, insira a senha de acesso: ");
+senha = Console.ReadLine();
+                
+    if (senha == "310704")
+    {
+        autenticado = true;
+    }
+    else
+    {
+         Console.WriteLine("Senha incorreta. Tente novamente.");
+     }
+} while (!autenticado);
+
+Console.WriteLine(@$"
+ ------------------------
+| 1 - Cadastrar passagem |
+| 2 - Listar Passagens   |
+| 0 - Sair               |
+ ------------------------                
+");
